c0c540b7b159584c337999a97f71829340bdd36e
Elad Ben-Haim <elad.benhaim42@gmail.com>
2022-02-03 18:02:29 +0200

    change url

--- setup.py
+++ setup.py
@@ -1,26 +1,11 @@
-from setuptools import setup
-
-setup(
-    name='buckets3',
-    version='0.1.0',    
-    description='A example Python package',
-    url='',
-    author='ODM',
-    packages=[],
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 3.8',
-    ],
-)
 import setuptools
 
 setuptools.setup(
-    name="bucketS3",
+    name="core",
     version="0.0.1",
     author="ODM",
     description="Read and write files to aws s3",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/EladBenHaim/core",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
